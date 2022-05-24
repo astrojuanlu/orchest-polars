61246e330816881282f3928d64a6520f39899115
Juan Luis Cano Rodríguez <juanlu@orchest.io>
2022-05-24 16:44:38 +0000

    Complete pipeline

---
+++ .orchest/environments/3da6aa3c-0b6d-4d37-a8ab-a2e21c63ee21/setup_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mamba install -c conda-forge -y python-graphviz
+
+pip install -r requirements.txt
---
+++ .orchest/environments/4c68d3ef-92d6-4415-aa29-45d989c72f76/setup_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt update && sudo -E apt install -y --no-install-recommends unzip
